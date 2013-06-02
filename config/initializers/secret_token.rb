58db90423e710a6fe11ddad6ba628dc907a88f0f
Freddy Bravo <fbravoxperia@gmail.com>
2013-06-02 15:35:55 -0500

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DEWTrabajopc01Adicional::Application.config.secret_token = '41f42541141f92b4e6598f645b5e67d6427b97cf3aca15ec2173446c45fa7966b121fd97266e119680486afd6786633031d3d8642955339de62d5e4b43f282e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DEWTrabajopc01Adicional::Application.config.session_store :cookie_store, key: '_DEW-TrabajoPC01-Adicional_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DEWTrabajopc01Adicional::Application.config.session_store :active_record_store
