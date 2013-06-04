0550ae3f37e3e153e9ab7b8308008b3709732b9b
Freddy Bravo <fbravoxperia@gmail.com>
2013-06-03 23:13:52 -0500

    actualizando

--- lastfm/config/initializers/lastfm.rb
+++
@@ -1,2 +0,0 @@
-LastFM.api_key     = "627218788bfd0060047d2fde5fa69ce9"
-LastFM.client_name = "RadioLastFM"
--- lastfm/lib/lastfm-client/api_classes/album.rb
+++
@@ -1,9 +0,0 @@
-module LastFM
-  class Album < APIClass
-    unrestricted_methods :get_buy_links, :get_info, :get_shouts, :get_tags, :get_top_tags, :search
-
-    restricted_methods do
-      write :add_tags, :remove_tag, :share
-    end
-  end
-end
